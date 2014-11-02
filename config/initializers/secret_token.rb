275292812c7c3486b4921562548b001b864b667c
Matt Adelberger <matt.adelberger@gmail.com>
2014-11-02 14:05:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Integrityhvacr::Application.config.secret_key_base = '4a84f6ee89029c7506196e05feee87e3a90c798f14a096eb55d28652281dd25ad8bceee0124c78243892e6516b20e953a9dc198912f1e6cec23769af11337e62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Integrityhvacr::Application.config.session_store :cookie_store, key: '_integrityhvacr_session'
